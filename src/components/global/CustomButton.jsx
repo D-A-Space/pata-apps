5d68ad1bdcde87c60989b8d7b184742bb28b5d71
AZZIE2000 <azzam.faraj@gmail.com>
2023-03-20 21:41:08 +0300

    start logic

--- src/components/global/CustomButton.jsx
+++ src/components/global/CustomButton.jsx
@@ -6,7 +6,7 @@ const CustomButton = ({ onclick, text }) => {
   return (
     <div
       onclick={onclick}
-      className="active:scale-[0.99] active:brightness-125  active:shadow-black  active:shadow-inner  cursor-pointer row-span-1 transform transition-all duration-200 h-full w-full bg-gradient-to-r select-none from-blue3 to-foshi flex justify-center items-center rounded-md text-white text-lg font-bold"
+      className="active:scale-[0.99] active:brightness-125  active:shadow-black  active:shadow-inner  cursor-pointer row-span-1 transform transition-all duration-200 h-full w-full bg-gradient-to-r select-none from-blue1 to-foshi flex justify-center items-center rounded-md text-white text-lg font-bold"
     >
       {text}
     </div>
--- src/components/quiz/QuestionInterface.jsx
+++ src/components/quiz/QuestionInterface.jsx
@@ -9,14 +9,14 @@ const QuestionInterface = () => {
     <div className="h-full w-full grid grid-rows-6 gap-1">
       <StatusBar />
       <QuestionField />
-      <div className="row-span-2 grid grid-rows-4 gap-3 p-3 px-5">
+      <div className="row-span-2 grid grid-rows-4 gap-3 p-3 px-1">
         <OptionButton />
         <OptionButton />
         <OptionButton />
         <OptionButton />
       </div>
       <div className="w-full flex justify-end items-center">
-        <div className="w-1/4 h-1/2">
+        <div className="w-2/6 h-2/6">
           <CustomButton text={"Next"} />
         </div>
       </div>
