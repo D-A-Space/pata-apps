708ebef2287098d7ae349a93d5892273bd9cd4db
DuaaNawwas <duaa.nawwas@gmail.com>
2023-03-20 21:29:53 +0300

    option button styles

--- src/components/quiz/components/OptionButton.jsx
+++ src/components/quiz/components/OptionButton.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { IoRadioButtonOffOutline } from "react-icons/io5";
+import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 
 const OptionButton = () => {
-  return <div className="row-span-1 bg-slate-800">OptionButton</div>;
+  const i = Math.random();
+  return (
+    <div key={Math.random()}>
+      <input
+        onChange={(e) => console.log(e)}
+        type="radio"
+        name="option"
+        className="peer hidden"
+        id={i}
+      />
+      <label
+        htmlFor={i}
+        className={`text-outer p-2 border-2 border-dashed border-outer peer-checked:border-solid peer-checked:border-candy peer-checked:text-candy peer-checked:[&>span:nth-of-type(1)]:hidden peer-checked:[&>span:nth-of-type(3)]:block  rounded-lg flex justify-between items-center`}
+        // className={`text-outer p-2 border-2 border-dashed border-outer peer-checked:border-solid  peer-checked:border-garden peer-checked:text-garden peer-checked:[&>span:nth-of-type(1)]:hidden peer-checked:[&>span:nth-of-type(2)]:block rounded-lg flex justify-between items-center`}
+      >
+        Option
+        <span>
+          <IoRadioButtonOffOutline size={25} />
+        </span>
+        <span className="hidden">
+          <AiOutlineCheckCircle size={25} />
+        </span>
+        <span className="hidden">
+          <AiOutlineCloseCircle size={25} />
+        </span>
+      </label>
+    </div>
+  );
 };
 
 export default OptionButton;
